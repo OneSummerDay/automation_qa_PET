352bcb89fa6ecd535c20537bce8e107d7115f54a
OneSummerDay <artem.kraievyi@gmail.com>
2022-12-06 12:40:52 +0200

    Created generator for fields

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -6,5 +6,11 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
-            print(text_box_page.check_filled_form())
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_curr_addr, output_per_addr = text_box_page.check_filled_form()
+            text_box_page.check_filled_form()
+            assert full_name == output_name
+            assert email == output_email
+            assert current_address == output_curr_addr
+            assert permanent_address == output_per_addr
+
